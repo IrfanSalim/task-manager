2058d5c99e4b004a87dbd6ad552957de261a50fa
Irfan <irfansalim77@gmail.com>
2023-09-08 22:28:54 +0530

    Added mongoDB connection and updated controllers and routes

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,10 +1,27 @@
-
-
-
 const getAllTasks = (req, res) => {
-    res.send("all tasks")
-}
+    res.send("all tasks");
+};
+
+const createTask = (req, res) => {
+    res.json(req.body);
+};
+
+const getTask = (req, res) => {
+    res.send("single tasks");
+};
+
+const updateTask = (req, res) => {
+    res.send("update tasks");
+};
+
+const deleteTask = (req, res) => {
+    res.send("delete tasks");
+};
 
 module.exports = {
     getAllTasks,
-}
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask,
+};
